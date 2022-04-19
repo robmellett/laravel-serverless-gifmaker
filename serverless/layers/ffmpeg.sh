df334a26752dd81feb4912436b63aed2d2729481
Rob Mellett <dev@robmellett.com>
2022-04-19 08:51:18 +0800

    Add layer

---
+++ serverless/layers/ffmpeg.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd 'serverless/layers'
+
+curl -O https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
+
+tar xf ffmpeg-git-amd64-static.tar.xz
+
+rm ffmpeg-git-amd64-static.tar.xz
+
+mv ffmpeg-git-*-amd64-static ffmpeg
